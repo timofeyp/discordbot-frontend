4d86be85119ded1711d1e0c7c36466a89822c014
timofeyp <timofeyp@live.com>
2019-01-06 19:06:41 +0300

    FIX webpack ENTRY

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: ['babel-polyfill', './src/reports.js'],
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'index-bundle.js'
